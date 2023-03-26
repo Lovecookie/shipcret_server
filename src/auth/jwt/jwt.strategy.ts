130c4820c283b232211dd5113f6e7f17cc16de8e
BaekSeungmin <silver2000cs@gmail.com>
2023-03-26 16:48:52 +0900

    jwt 인증처링 controller 영역으로 변경!

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,30 +1,36 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/contents/users/users.service';
 import { FDatabaseConstants } from 'src/database/database.constants';
 import { FUserEntity } from 'src/database/entitys/users.entity';
 import { FUserRepository } from 'src/database/repositorys/user.repository';
-import { FPayload } from './jwt.payload';
+import { FJwtPayload } from './jwt.payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        @Inject(FDatabaseConstants.USER_REPOSITORY)
-        private readonly userRepository: FUserRepository,
-    ) {
+    constructor(private readonly usersService: UsersService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET,
+            secretOrKey: process.env.JWT_SECRET
         });
     }
 
-    async validate(payload: FPayload): Promise<FUserEntity> {
-        const userEntity = await this.userRepository.findOneById(payload.sub);
-        if (userEntity) {
-            return userEntity;
-        } else {
-            throw new UnauthorizedException('jwt is invalid!');
+    async validate(payload: FJwtPayload): Promise<FUserEntity> {
+        try {
+            console.log('JwtStrategy validate()');
+            const userEntity = await this.usersService.findOneByPayload(
+                payload.aud
+            );
+            if (userEntity) {
+                return userEntity;
+            } else {
+                throw new UnauthorizedException('jwt is invalid!');
+            }
+        } catch (error) {
+            console.log('JwtStrategy UnauthorizedException()');
+            throw new UnauthorizedException(error);
         }
     }
 }
